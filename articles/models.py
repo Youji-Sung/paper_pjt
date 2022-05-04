20337f2843fc525783d657f1a1d5be5caa15f462
Lilac-s <anc9432@gmail.com>
2022-05-05 02:09:39 +0900

    start rollingpaper

---
+++ articles/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.conf import settings
+from django.forms import CharField
+
+# Create your models here.
+class Article(models.Model):
+    nickname = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like = models.IntegerField()
+    password = models.CharField(max_length=100)
+    article_img = models.ImageField()
+
+    def __str__(self):
+            return self.nickname
+
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
+    nickname = CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+            return self.content
---
+++ articles/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'articles'
+urlpatterns = [
+    # 그냥 만들어 놓았으니 수정하세요~
+]
