c6baa962d9f47f4428694a6bc81407c1755031ff
neweastK <eastshin2021@gmail.com>
2022-05-11 00:51:22 +0900

    merge test
    merge test를 위한 commit

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     # 그냥 만들어 놓았으니 수정하세요~
-    path('<int:article_pk>/detail/', views.detail, name='detail'),
+    path('<int:article_pk>/detail/comments', views.comments_create, name='comment_create'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def detail(request,article_pk):
+    pass
